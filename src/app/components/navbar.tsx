445fc6c25cf7d876cc3a1e35a3e18f1613b83de4
krushnal07 <krushnalpatil2955@gmail.com>
2025-06-19 13:24:37 +0530

    error solved

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -2,7 +2,7 @@
 import React, { useState } from "react";
 import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
 import { cn } from "../utils/cn";
-import Link from "next/link";
+//import Link from "next/link";
 
 export function NavbarDemo() {
   return (
